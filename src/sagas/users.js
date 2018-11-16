9239e19b1d789d3d10e36963b37d4317c96fa1a6
mitsunori.morita <mitsunori.morita@speee.jp>
2018-11-16 11:28:11 +0900

    Add users saga with call, fork and all

--- src/sagas/users.js
+++ src/sagas/users.js
@@ -1,11 +1,19 @@
 import { takeEvery } from "redux-saga/effects";
+import { takeEvery, call, fork } from "redux-saga/effects";
 import * as actions from "../actions/users";
+import * as api from "../api/users";
 
 function* getUsers() {
   try {
+    const result = yield call(api.geUsers);
+    console.log(result);
   } catch (e) {}
 }
 
 function* watchGetUsersRequest() {
   yield takeEvery(actions.Types.GET_USERS_REQUEST, getUsers);
 }
+
+const usersSagas = [fork(watchGetUsersRequest)];
+
+export default usersSagas;
