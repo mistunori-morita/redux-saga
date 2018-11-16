9ed6f9080e27a15dcadedbc01d468dbee54f973e
mitsunori.morita <mitsunori.morita@speee.jp>
2018-11-16 16:40:36 +0900

    Add UsersList components

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getUsersRequest } from "../actions/users";
+import UserList from "./UsersList";
 class App extends Component {
   constructor(props) {
     super(props);
     this.props.getUsersRequest();
   }
   render() {
-    return <div>aa</div>;
+    const users = this.props.users;
+    return (
+      <div style={{ margin: "0 auto", padding: "20px", maxWidth: "600px" }}>
+        <UserList users={users.items} />
+      </div>
+    );
   }
 }
 
 export default connect(
-  null,
+  ({ users }) => ({ users }),
   { getUsersRequest }
 )(App);
