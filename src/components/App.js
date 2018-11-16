e20e8bf5f5b20a41e1f0ec24a0d77d47e50aafee
mitsunori.morita <mitsunori.morita@speee.jp>
2018-11-16 10:57:43 +0900

    Add generator functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
 
+function* testing() {
+  while (true) {
+    yield 1;
+    yield 2;
+    yield 3;
+  }
+}
+
 class App extends Component {
   render() {
-    return <div>test</div>;
+    const iterator = testing();
+    console.log(iterator.next());
+    console.log(iterator.next());
+    console.log(iterator.next());
+    console.log(iterator.next());
+
+    return <div>aa</div>;
   }
 }
 
