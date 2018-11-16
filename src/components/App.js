5f3874347bc1a6aed3119147b402477034568809
mitsunori.morita <mitsunori.morita@speee.jp>
2018-11-16 18:05:22 +0900

    Add new form Components

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,21 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getUsersRequest } from "../actions/users";
 import UserList from "./UsersList";
+import NewUserForm from "./NewUserForm";
 class App extends Component {
   constructor(props) {
     super(props);
     this.props.getUsersRequest();
   }
+
+  handleSubmit = ({ firstName, lastName }) => {
+    console.log(firstName, lastName);
+  };
   render() {
     const users = this.props.users;
     return (
       <div style={{ margin: "0 auto", padding: "20px", maxWidth: "600px" }}>
+        <NewUserForm onSubmit={this.handleSubmit} />
         <UserList users={users.items} />
       </div>
     );
