467a04004914cccae6b964824cbbfa48ec99b3cc
mitsunori.morita <mitsunori.morita@speee.jp>
2018-11-16 11:52:50 +0900

    connecting it all together

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
-
+import { connect } from "react-redux";
+import { getUsersRequest } from "../actions/users";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.props.getUsersRequest();
+  }
   render() {
     return <div>aa</div>;
   }
 }
 
-export default App;
+export default connect(
+  null,
+  { getUsersRequest }
+)(App);
